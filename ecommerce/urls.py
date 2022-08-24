aa408598c00da428dc846299d90457753bed2252
franhidalgocavs <61721723+franhidalgocavs@users.noreply.github.com>
2022-08-24 20:57:44 -0300

    hice lo que faltaba del video anterior redux instalacion terminada

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("vistaprevia.urls")),
+    path("vistaprevia", include("vistaprevia.urls")),
+    path("accounts/", include("registration.backends.default.urls")),
     path("admin/", admin.site.urls),
 ]
